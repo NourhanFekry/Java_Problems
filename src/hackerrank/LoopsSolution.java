f5f4bc021fe848f211355fb4a361009e50612aca
NourhanFekry <nourhanfekry28>
2024-08-03 03:32:28 +0300

    loop Solution

---
+++ src/hackerrank/LoopsSolution.java
@@ -0,0 +1,25 @@
+package hackerrank;
+import java.io.*;
+
+
+public class LoopsSolution {
+    /**
+     Given an integer,n, print its first 10 multiples.
+     Each multiple n*i (where 1<=i<=10 )
+     should be printed on a new line in the form: N x i = result.
+     * **/
+
+    public static class Solution {
+        public static void main(String[] args) throws IOException {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            int n = Integer.parseInt(bufferedReader.readLine().trim());
+            for (int i=1;i<=10;i++)
+            {
+                System.out.println(n + " x " + i + " = " + n*i);
+            }
+
+            bufferedReader.close();
+        }
+    }
+}
