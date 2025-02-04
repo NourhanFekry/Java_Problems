6d72842e62c09f81f0c43dc7b7b4ba6bffae84fa
NourhanFekry <nourhanfekry28>
2025-02-04 03:56:14 +0200

    Update the Solution for single Number

--- src/Problems/SingleNumberProblem.java
+++ src/Problems/SingleNumberProblem.java
@@ -1,6 +1,11 @@
 package Problems;
 
 public class SingleNumberProblem {
+    public static void main(String[] args) {
+        int[] arrry = {2, 2, 1};
+        System.out.println(singleNumber(arrry));
+
+    }
 
     /*Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 
@@ -9,7 +14,7 @@ public class SingleNumberProblem {
     //Answer
 
 
-    public int singleNumber(int[] nums) {
+    public static int singleNumber(int[] nums) {
 
         int result = 0;
         for (int num : nums) {
