5407a25edd51c36a9ed2c2c232d3944e12b18fa6
NourhanFekry <nourhanfekry28>
2025-02-04 03:51:56 +0200

    35 Search Insert Position Solution

---
+++ src/Problems/SearchInsertPosition.java
@@ -0,0 +1,30 @@
+package Problems;
+
+public class SearchInsertPosition {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 6};
+        int index = searchInsert(nums, 5);
+        System.out.println("the target is "+ index );
+
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+}
+
